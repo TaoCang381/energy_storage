18087e1746749ce327df9789d8613a96c64b67f9
unknown <1114733816@qq.com>
2025-07-29 19:09:48 +0800

    first commit

--- high power density group/flywheel_simulation.py
+++ high power density group/flywheel_simulation.py
@@ -2,8 +2,6 @@ import math
 import matplotlib.pyplot as plt
 
 import numpy as np
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体为黑体
-plt.rcParams['axes.unicode_minus'] = False   # 解决保存图像是负号'-'显示为方块的问题
 
 class FlywheelModel:
     """飞轮储能系统模型"""
